b5d569ef31d5470bcd5b60303eb046d168fcf1b8
林明憲 <87749297+Hsien2000@users.noreply.github.com>
2025-03-13 16:48:20 +0800

    2025_03_10

---
+++ 114_03_06/Program5_13/Program5_13/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace Program5_13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();//亂數
+            StreamWriter outputFile;
+            int count;
+
+            try
+            {
+                if (saveFile.ShowDialog() == DialogResult.OK) 
+                {
+                    outputFile = File.CreateText(saveFile.FileName);
+                    if (int.TryParse(textBox1.Text, out count))
+                    {
+                        for (int i = 0; i < count; i++)
+                        {
+                            outputFile.WriteLine(rand.Next(100) + 1);
+
+                        }
+                        outputFile.Close();
+                        MessageBox.Show("已建檔");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("請輸入數字");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("按了取消");
+                }
+
+                
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);//顯示錯誤訊息
+            }
+        }
+    }
+}
---
+++ 114_03_06/Program5_14 _1/Program5_14/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Program5_14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader inputFile; // 宣告StreamReader物件
+            int sum = 0; // 宣告變數sum用來存放總和
+            int count = 0; // 宣告變數count用來存放數字個數
+            int temp = 0; // 宣告變數temp用來存放讀取的數字
+            try
+            {
+                inputFile = File.OpenText("numbers.txt"); // 開啟檔案
+                while (!inputFile.EndOfStream) // 檔案未結束
+                {
+                    count++; // 計算數字個數
+                    temp = int.Parse(inputFile.ReadLine()); // 將讀取的數字存入temp
+                    sum += temp; // 將數字加總
+                    listBox1.Items.Add(temp);// 將資料加入ListBox
+                }
+                listBox1.Items.Add("總共有: " + count+"個數字\n總和"+sum); // 將總和加入ListBox
+                inputFile.Close(); // 關閉檔案
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // 顯示錯誤訊息    
+            }
+        }
+    }
+}
